427158fb153d1a7f60a47a30a060b18d26b6500f
Katia Sujey <233351@ids.upchiapas.edu.mx>
2024-07-22 13:44:18 -0600

    commit base

--- src/user/repositories/UserRepository.ts
+++ src/user/repositories/UserRepository.ts
@@ -6,12 +6,12 @@ export class UserRepository {
 
   public static async findAll(): Promise<User[]> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT user_id, role_id, name, address, phone_number, age, username, password, created_at, updated_at, deleted FROM users', (error: any, results) => {
+      connection.query('SELECT user_id, role_id, name, address, phone_number, age, username, password, created_at, updated_at, deleted FROM user', (error: any, results) => {
         if (error) {
           reject(error);
         } else {
-          const users: User[] = results as User[];
-          resolve(users);
+          const user: User[] = results as User[];
+          resolve(user);
         }
       });
     });
@@ -19,13 +19,13 @@ export class UserRepository {
 
   public static async findById(user_id: number): Promise<User | null> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM users WHERE user_id = ?', [user_id], (error: any, results) => {
+      connection.query('SELECT * FROM user WHERE user_id = ?', [user_id], (error: any, results) => {
         if (error) {
           reject(error);
         } else {
-          const users: User[] = results as User[];
-          if (users.length > 0) {
-            resolve(users[0]);
+          const user: User[] = results as User[];
+          if (user.length > 0) {
+            resolve(user[0]);
           } else {
             resolve(null);
           }
@@ -36,13 +36,13 @@ export class UserRepository {
 
   public static async findByUsername(username: string): Promise<User | null> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM users WHERE username = ?', [username], (error: any, results) => {
+      connection.query('SELECT * FROM user WHERE username = ?', [username], (error: any, results) => {
         if (error) {
           reject(error);
         } else {
-          const users: User[] = results as User[];
-          if (users.length > 0) {
-            resolve(users[0]);
+          const user: User[] = results as User[];
+          if (user.length > 0) {
+            resolve(user[0]);
           } else {
             resolve(null);
           }
@@ -52,7 +52,7 @@ export class UserRepository {
   }
 
   public static async createUser(user: User): Promise<User> {
-    const query = 'INSERT INTO users (name, address, phone_number, age, role_id, username, password, created_at, updated_at, deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const query = 'INSERT INTO user (name, address, phone_number, age, role_id, username, password, created_at, updated_at, deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     return new Promise((resolve, reject) => {
       connection.execute(query, [user.name, user.address, user.phone_number, user.age, user.role_id, user.username, user.password, user.created_at, user.updated_at, user.deleted], (error, result: ResultSetHeader) => {
         if (error) {
@@ -67,7 +67,7 @@ export class UserRepository {
   }
 
   public static async updateUser(user_id: number, userData: User): Promise<User | null> {
-    const query = 'UPDATE users SET name = ?, address = ?, phone_number = ?, age = ?, role_id = ?, username = ?, password = ?, updated_at = ?, deleted = ? WHERE user_id = ?';
+    const query = 'UPDATE user SET name = ?, address = ?, phone_number = ?, age = ?, role_id = ?, username = ?, password = ?, updated_at = ?, deleted = ? WHERE user_id = ?';
     return new Promise((resolve, reject) => {
       connection.execute(query, [userData.name, userData.address, userData.phone_number, userData.age, userData.role_id, userData.username, userData.password, userData.updated_at, userData.deleted, user_id], (error, result: ResultSetHeader) => {
         if (error) {
@@ -85,7 +85,7 @@ export class UserRepository {
   }
 
   public static async deleteUser(user_id: number): Promise<boolean> {
-    const query = 'DELETE FROM users WHERE user_id = ?';
+    const query = 'DELETE FROM user WHERE user_id = ?';
     return new Promise((resolve, reject) => {
       connection.execute(query, [user_id], (error, result: ResultSetHeader) => {
         if (error) {
