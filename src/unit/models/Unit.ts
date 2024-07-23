44d9414326f3dfb026f10eeecd5ee8018fb9ff43
Katia Sujey <233351@ids.upchiapas.edu.mx>
2024-07-23 10:02:32 -0600

    commit base.

---
+++ src/unit/models/Unit.ts
@@ -0,0 +1,11 @@
+export interface Unit {
+    unit_id: number;
+    name: string;
+    series: string;
+    phone_number: string;
+    marca: number;
+    placa: number;
+    created_at: string;
+    updated_at: string;
+    deleted: boolean; 
+}
