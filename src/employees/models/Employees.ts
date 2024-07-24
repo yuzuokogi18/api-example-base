f86b4b6b0481385ae855fb53838630a1b7a3cd3b
Katia Sujey <233351@ids.upchiapas.edu.mx>
2024-07-23 21:10:38 -0600

    commit base

---
+++ src/employees/models/Employees.ts
@@ -0,0 +1,10 @@
+export interface Employees {
+    employees_id: number;
+    name: string;
+    position: string;
+    department: number;
+    user_id: number;
+    created_at: string;
+    updated_at: string;
+    deleted: boolean; 
+}
