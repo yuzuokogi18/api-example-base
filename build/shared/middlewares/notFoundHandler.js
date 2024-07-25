96476c45ad2148a81c5e711374f7e96e8cc1b3a2
Katia Sujey <233351@ids.upchiapas.edu.mx>
2024-07-25 12:48:09 -0600

    remove build

--- build/shared/middlewares/notFoundHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-function notFoundHandler(_req, res, _next) {
-    res.status(404).json({ error: 'Ruta no encontrada' });
-}
