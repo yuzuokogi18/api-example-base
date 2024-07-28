537439463fd998a7af4838ef3dca60e3bdb789e8
Katia Sujey <233351@ids.upchiapas.edu.mx>
2024-07-28 10:06:09 -0600

    commit base

--- src/user_unit/routes/user_unitRoutes.ts
+++ src/user_unit/routes/user_unitRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { getUserUnits, getUserUnitById, createUserUnit, updateUserUnit, deleteUserUnit } from '../controllers/user_unitcontrollers';
+import { getUserUnits, getUserUnitById, createUserUnit, updateUserUnit, deleteUserUnit } from '../controllers/user_unitControllers';
 import { authMiddleware } from '../../shared/middlewares/auth';
 
 const userUnitRoutes: Router = Router();
 
-userUnitRoutes.get('/', getUserUnits);
+userUnitRoutes.get('/', authMiddleware, getUserUnits);
 userUnitRoutes.get('/:user_unit_id', authMiddleware, getUserUnitById);
-userUnitRoutes.post('/', createUserUnit);
-userUnitRoutes.put('/:user_unit_id', updateUserUnit);
-userUnitRoutes.delete('/:user_unit_id', deleteUserUnit);
+userUnitRoutes.post('/', authMiddleware, createUserUnit);
+userUnitRoutes.put('/:user_unit_id', authMiddleware, updateUserUnit);
+userUnitRoutes.delete('/:user_unit_id', authMiddleware, deleteUserUnit);
 
 export default userUnitRoutes;
